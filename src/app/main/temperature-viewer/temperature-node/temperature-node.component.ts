e451108d5952d6d8ea3a21f04c0cc66587cf5e38
Lim Chee Keat <ckeat0203@gmail.com>
2024-07-15 13:58:54 +0800

    try signal

--- src/app/main/temperature-viewer/temperature-node/temperature-node.component.ts
+++ src/app/main/temperature-viewer/temperature-node/temperature-node.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, Input, OnChanges, Signal, SimpleChanges} from '@angular/core';
+import {Component, computed, input, Input, OnChanges, Signal, SimpleChanges} from '@angular/core';
 import {DecimalPipe} from "@angular/common";
 
 @Component({
@@ -11,17 +11,23 @@ import {DecimalPipe} from "@angular/common";
   styleUrl: './temperature-node.component.scss'
 })
 export class TemperatureNodeComponent implements OnChanges {
-  @Input() temperatures: Array<number> | undefined;
-  @Input() roomName: string | undefined;
+  // @Input() temperatures: Array<number> | undefined;
+  // @Input() roomName: string | undefined;
 
-  averageTemperature = 0;
-  totalTemperatureCount = 0;
+  temperatures = input<Array<number>>([])
+  roomName = input<string>("")
+
+  averageTemperature: Signal<number> = computed(() => this.temperatures().reduce((sum, current) => sum + current, 0) / this.temperatures().length);
+  totalTemperatureCount = computed(() => this.temperatures().length);
+
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   if (this.temperatures()){
+  //     this.averageTemperature = this.temperatures().reduce((sum, current) => sum + current, 0) / this.temperatures.length;
+  //     this.totalTemperatureCount = this.temperatures.length;
+  //   }
+  // }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (this.temperatures){
-      this.averageTemperature = this.temperatures.reduce((sum, current) => sum + current, 0) / this.temperatures.length;
-      this.totalTemperatureCount = this.temperatures.length;
-    }
   }
 }
 
